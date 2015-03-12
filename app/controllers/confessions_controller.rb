aa940e78210cba9c6d69921e11450a52475bdaa1
Johnny Ji <johnny.ji@live.ca>
2015-03-12 13:05:15 -0700

    initial commit

---
+++ app/controllers/confessions_controller.rb
@@ -0,0 +1,19 @@
+class ConfessionsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+end
